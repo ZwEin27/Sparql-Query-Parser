0c6bee021e8f6fca1aa992c9da18764f67605a62
ZwEin <zwein27@gmail.com>
2016-07-20 00:04:53 -0400

    update command line script

--- sqparser/__init__.py
+++ sqparser/__init__.py
@@ -2,9 +2,10 @@
 # @Author: ZwEin
 # @Date:   2016-07-19 19:16:31
 # @Last Modified by:   ZwEin
-# @Last Modified time: 2016-07-19 23:29:01
+# @Last Modified time: 2016-07-20 00:04:42
 
 import re
+import json
 
 ######################################################################
 #   Constant
@@ -34,7 +35,7 @@ SQ_EXT_CLAUSES = 'clauses'
 SQ_EXT_PREDICATE = 'predicate'
 SQ_EXT_CONSTAINT = 'constraint'
 SQ_EXT_FILTERS = 'filters'
-SQ_EXT_OPTIONAL = 'optional'
+SQ_EXT_OPTIONAL_FLAG = 'isOptional'
 
 ######################################################################
 #   Regular Expression
@@ -45,6 +46,10 @@ re_brackets_least = re.compile(r'(?<={).*?(?=})')
 
 reg_outer = r'(?:'+r'|'.join(SQ_OUTER_KEYWORDS)+r').*?(?='+r'|'.join(SQ_OUTER_KEYWORDS)+r'|\s*$)'
 re_outer = re.compile(reg_outer)
+reg_inner = r'(?:'+r'|'.join(SQ_INNER_KEYWORDS)+r').*?(?='+r'|'.join(SQ_INNER_KEYWORDS)+r'|\s*$)'
+re_inner = re.compile(reg_inner)
+
+re_keyword = re.compile(r'^[a-zA-Z]+\b')
 
 # statement
 re_statement_split = re.compile(r'[;\.]')
@@ -102,20 +107,35 @@ class SQParser(object):
         return ans
 
 
+
     @staticmethod
     def parse_statement(ans, text):
         if re_statement_a.search(text):
+            print 'a'
             # for a
             ans[SQ_EXT_TYPE] = re_statement_qpr.search(text).group(0)
             ans[SQ_EXT_VARIABLE] = re_statement_variable.search(text).group(0)
-        elif SQ_KEYWORD_FILTER in text:
-            pass
-        elif SQ_KEYWORD_OPTIONAL in text:
-            pass
-            # re_brackets_most.search(_).group(0) if re_brackets_most.search(_) else _
+        elif len(re_inner.findall(text)) > 0:
+            print 'b'
+            # components = {re_keyword.match(_).group(0):re_brackets_most.search(_).group(0) if re_brackets_most.search(_) else _ for _ in re_inner.findall(statement)}
+
+            for component in re_inner.findall(text):
+                keyword = re_keyword.match(component).group(0)
+                content = re_brackets_most.search(component).group(0) if re_brackets_most.search(component) else component
+                if keyword == SQ_KEYWORD_OPTIONAL:
+                    content = re_statement_content.search(content).group(0)
+                    if not content:
+                        raise Exception('Sparql Format Error')
+                    clause = SQParser.parse_content(content)
+                    clause[SQ_EXT_OPTIONAL_FLAG] = True
+                    ans[SQ_EXT_CLAUSES].append(clause)
+                elif keyword == SQ_KEYWORD_FILTER:
+                    pass
+                else:
+                    pass
         else:
+            print 'c'
             content = re_statement_content.search(text)
-            # print content.group()
             if not content:
                 raise Exception('Sparql Format Error')
             content = content.group(0)
@@ -139,7 +159,8 @@ class SQParser(object):
         #     t = components[target_component]
         #     print t
 
-        print SQParser.parse_components(components)[target_component]
+        ans = SQParser.parse_components(components)[target_component]
+        print json.dumps(ans, indent=4)
 
 
 
@@ -148,6 +169,24 @@ class SQParser(object):
 
 
 if __name__ == '__main__':
-    # text = "PREFIX qpr: <http://istresearch.com/qpr> SELECT ?business  (count(?ad) AS ?count)(group_concat(?ad;separator=',') AS ?ads)   WHERE { ?ad a qpr:Ad ; qpr:location 'Fargo, ND' ; qpr:business_type ?bt . FILTER(?bt = 'Spa' || ?bt = 'Massage Parlor') ?ad qpr:services 'sex' . OPTIONAL { ?ad qpr:business_name ?business_name} OPTIONAL { ?ad qpr:physical_address ?physical_address } BIND( IF(BOUND(?business_name) && BOUND(?physical_address), CONCAT(?business_name, \",\", ?physical_address), IF(BOUND(?business_name), ?business_name, ?physical_address)) AS ?business) } GROUP BY ?business ORDER BY ?count LIMIT 1"
+    """
     text = "PREFIX qpr: <http://istresearch.com/qpr> SELECT ?cluster ?ad WHERE { ?cluster a qpr:cluster ; qpr:seed '5105124396' ; qpr:ad ?ad . OPTIONAL { ?ad qpr:image_with_phone ?iwp } OPTIONAL { ?ad qpr:image_with_email ?iwe } FILTER(bound(?iwp) || bound(?iwe) || ?bt = 'Spa') }"
+    
     SQParser.parse(text)
+    """
+
+    import sys
+    import argparse
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-t','--text', required=True)
+
+    args = arg_parser.parse_args()
+
+    text = str(args.text)
+    print SQParser.parse(text)
+
+    """
+
+
+
